239e18bd1c22bc40ddc9df2a40aad9ec43306985
Arnav Karnik <arnav.karnik@gmail.com>
2025-02-07 08:35:58 +0530

    test

--- Year3/Parallel_Computer_Architecture_and_Programming_Lab/Week4/q2.c
+++ Year3/Parallel_Computer_Architecture_and_Programming_Lab/Week4/q2.c
@@ -4,9 +4,11 @@
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
+
     int rank, nop, search;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &nop);
+
     MPI_Status stat;
     int mat[3][3];
     int temp[3];
@@ -20,18 +22,22 @@ int main(int argc, char* argv[]) {
         printf("Enter the number to be searched:\n");
         scanf("%d",&search);
     }
+
     MPI_Bcast(&search, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Scatter(mat, 3, MPI_INT, temp, 3, MPI_INT, 0, MPI_COMM_WORLD);
     int occ = 0, sum_occ = 0;
+
     for(int i = 0; i < 3; i++) {
-        if(search == temp[i]) {
+        if (search == temp[i]) {
             occ++;
         }
     }
+
     MPI_Reduce(&occ, &sum_occ, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     if(rank == 0) {
         printf("Total Number of Occurences: %d\n", sum_occ);
     }
+    
     MPI_Finalize();
     exit(0);
 } 
